863d463f81476cc0c043d594102249f75d97fd17
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-01-28 12:24:02 +0100

    gli attori vengono loggati in console al click di un bottone

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
+    const apiUrl = "https://lanciweb.github.io/demo/api/actresses/";
+
+    const printActors = () => {
+        axios.get(apiUrl).then((res) => console.log(res.data));
+    };
+
     return (
         <>
-            <div>Hello React World!</div>
+            <div>Lista di attori</div>
+            <ul></ul>
+            <button onClick={printActors}>Stampa attori</button>
         </>
-    )
+    );
 }
 
 export default App;
