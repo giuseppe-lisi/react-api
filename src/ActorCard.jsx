b321c5509d0a57fbd27961c2cb35a81c380946d8
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-01-28 13:11:02 +0100

    cambiato testo in inglese

--- src/ActorCard.jsx
+++ src/ActorCard.jsx
@@ -1,13 +1,13 @@
 function ActorCard({ actor }) {
     return (
-        <li key={actor.id}>
+        <li key={actor.id} className="card">
             <div>
                 <img src={actor.image} alt={`Foto di ${actor.name}`} />
-                <h2>Nome: {actor.name}</h2>
-                <h5>Anno di Nascita: {actor.birth_year}</h5>
-                <h5>Nazionalità: {actor.nationality}</h5>
-                <p>Biografia: {actor.biography}</p>
-                <sub>Riconoscimenti: {actor.awards}</sub>
+                <h2>Name: {actor.name}</h2>
+                <h5>Birthyear: {actor.birth_year}</h5>
+                <h5>Nationality: {actor.nationality}</h5>
+                <p>Bio: {actor.biography}</p>
+                <sub>Awards: {actor.awards}</sub>
             </div>
         </li>
     );
