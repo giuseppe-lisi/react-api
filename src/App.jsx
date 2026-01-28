7df80d2b1d10caa4905e47120e646f66def1c394
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-01-28 13:57:35 +0100

    vengono ora stampate due liste, una con attori donne e una con attori uomini

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,33 @@ import ActorCard from "./ActorCard";
 import axios from "axios";
 
 function App() {
-    const apiUrl = "https://lanciweb.github.io/demo/api/actresses/";
-    const [actorList, setActorList] = useState([]);
+    const apiUrlWomen = "https://lanciweb.github.io/demo/api/actresses/";
+    const apiUrlMen = "https://lanciweb.github.io/demo/api/actors/";
+
+    const [womenActorList, setWomenActorList] = useState([]);
+    const [menActorList, setMenActorList] = useState([]);
 
     useEffect(() => {
-        axios.get(apiUrl).then((res) => {
-            setActorList(res.data);
+        axios.get(apiUrlWomen).then((res) => {
+            setWomenActorList(res.data);
         });
     }, []);
 
-    console.log(actorList);
+    useEffect(() => {
+        axios.get(apiUrlMen).then((res) => {
+            setMenActorList(res.data);
+        });
+    }, []);
 
     return (
         <>
             <div>Lista di attori</div>
             <ul className="cardContainer">
-                {actorList.map((actor, i) => {
-                    return <ActorCard actor={actor}/>;
+                {womenActorList.map((actor, i) => {
+                    return <ActorCard actor={actor} />;
+                })}
+                {menActorList.map((actor, i) => {
+                    return <ActorCard actor={actor} />;
                 })}
             </ul>
         </>
